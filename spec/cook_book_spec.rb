1e64e6d0c5894b00c29e054809f3c3deb4c7d207
Jason Noble <jason.noble@itron.com>
2022-08-02 09:54:31 -0600

    Add Cookbook class via TDD

---
+++ spec/cook_book_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+require './lib/cook_book'
+require './lib/recipe'
+
+RSpec.describe CookBook do
+  let(:recipe1) { Recipe.new("Mac and Cheese") }
+  let(:recipe2) { Recipe.new("Cheese Burger") }
+  let(:cookbook) { described_class.new }
+
+  describe '#new' do
+    it 'creates a CookBook' do
+      expect(cookbook).to be_a(described_class)
+    end
+  end
+
+  describe '#add_recipe(recipe)' do
+    it 'adds the recipe to the cookbook' do
+      expect{cookbook.add_recipe(recipe1)}.to change(cookbook.recipes, :count).from(0).to(1)
+    end
+  end
+end
